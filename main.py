69e7472488264114c29dad4ebb04149d32ae3593
unknown <donkgz@gmail.com>
2024-03-13 22:54:24 -0600

    williston_polygon_1 added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import csv
 import math
 import random
+import os
 import matplotlib.pyplot as plt
 from scipy.spatial import QhullError
 from scipy.interpolate import griddata
@@ -13,6 +14,8 @@ from google.cloud import bigquery
 
 # Replace 'your_project_id' with your actual Google Cloud project ID
 # Replace 'your_dataset' and 'your_table' with your dataset and table names
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\ltp\directionals_in_3d\siros-bq.json"
 project_id = 'siros-tech'
 dataset_id = 'develop'
 table_id = 'lg_directionals_24_02'
@@ -115,7 +118,7 @@ def get_elevation(api_10):
             if row[2] and row[0].startswith(str(api_10)):
                 # If a match is found, return the value from the Latitude column (index 52)
                 print(f"Elevation: {row[1]}, KB elev: {row[2]}")
-                return int(row[2])  # Adjust the index if necessary based on the actual CSV structure
+                return int(float(row[2]))  # Adjust the index if necessary based on the actual CSV structure
             # else:
                 # print("asdasd")
         print(f"No KB elevation found for: {api_10}")
@@ -417,8 +420,8 @@ def get_wellbores_from_polygon(polygon) :
                 'POLYGON(({polygon}))'), 
             ST_GEOGPOINT(longitude, latitude)) 
         AND tvd IS NOT NULL 
-        AND ( wellbore LIKE "LAT%" OR wellbore LIKE "STK%" )
-        # OR wellbore LIKE "DIR" )    
+        AND ( wellbore LIKE "LAT%" OR wellbore LIKE "STK%" 
+        OR wellbore LIKE "DIR" )    
         order by api_10, md
     """
     query_job = client.query(query)
@@ -516,6 +519,7 @@ def random_color():
     colors = [  'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
             'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
             'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']
+    # colors.append(['Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Grays', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_grey', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gist_yerg', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'grey', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r'])
     return colors[random.randint(0,len(colors)-1)]
 
 
@@ -595,6 +599,17 @@ polygon_shaply = Polygon([
     (-103.64816832746658,  47.99327398116597),
     (-103.64803185718695, 48.0246192603926)
     ])
+
+polygon_bq = "-102.51434552586815 47.78966776883909,-102.40718073138275 47.789729842381846,-102.40704932435575 47.77504854184551,-102.51434615693474 47.77543016818139,-102.51699071767595 47.786859697481646,-102.51434552586815 47.78966776883909"
+polygon_shaply = Polygon([
+    (-102.51434552586815, 47.78966776883909),
+    (-102.40718073138275, 47.789729842381846),
+    (-102.40704932435575, 47.77504854184551),
+    (-102.51434615693474, 47.77543016818139),
+    (-102.51699071767595, 47.786859697481646),
+    (-102.51434552586815, 47.78966776883909)
+    ])
+
 surface_codes = ["UB","PRNG"]
 polygon_extended = get_scaled_polygon(polygon_shaply,3)
 P_wellbores = get_wellbores_from_polygon(polygon=polygon_bq)
